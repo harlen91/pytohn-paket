48da043cc9d0176b90f73df35854bb4f4cf9aaee
harlen gilbert simanullang <harlengilbertsimanullang@MacBook-Pro-harlen.local>
2025-07-23 12:57:56 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import sains
+
+x=2
+y=4
+print(sains.kali(x,y))
---
+++ sains/geometry.py
@@ -0,0 +1,13 @@
+def luas_persegi_panjang(panjang, lebar):
+    return panjang * lebar
+
+def keliling_persegi_panjang(panjang, lebar):
+    return 2 * (panjang + lebar)
+
+def luas_lingkaran(jari_jari):
+    import math
+    return math.pi * jari_jari**2
+
+def keliling_lingkaran(jari_jari):
+    import math
+    return 2 * math.pi * jari_jari
